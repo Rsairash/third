d3b4cb349f29d09adec0f9d0d7ed2927e08f48eb
Sairash <sairash0323@gmail.com>
2021-07-20 08:04:02 +0545

    sum of matrix program

--- third.c
+++ third.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+int matrix();
 int main()
 {
     int t1=0,t2=1,i,n,NextTerm;
@@ -15,6 +16,68 @@ int main()
         NextTerm=t1+t2;
 
     }
+    
+    matrix();
     return 0;
 
+}
+
+int matrix()
+{
+    int i,j,n,a[10][10],b[10][10],sum[10][10];
+    int row,column;
+    printf("\nEnter the row and column: \n \
+Row: ");
+    scanf("%d",&row);
+    printf("Column: ");
+    scanf("%d",&column);
+
+    printf("Enter the elements of first matrix:\n");
+
+    for (i=0;i<row;i++)
+    {
+        for(j=0;j<column;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Enter the elements of second matrix:\n");
+
+    for (i=0;i<row;i++)
+    {
+        for(j=0;j<column;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    printf("The matrix are: \n");
+    printf("First matrix\n");
+    for (i=0;i<row;i++)
+    {
+        for(j=0;j<column;j++)
+        {
+            printf("\t%d",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("second matrix: \n");
+    for (i=0;i<row;i++)
+    {
+        for(j=0;j<column;j++)
+        {
+            printf("\t%d",b[i][j]);
+        }
+        printf("\n");
+    }
+    printf("The sum of matrix is \n");
+    for (i=0;i<row;i++)
+    {
+        for(j=0;j<column;j++)
+        {
+            sum[i][j]= a[i][j]+b[i][j];
+            printf("\t%d",sum[i][j]);
+        }
+        printf("\n");
+    }
+    
 }
